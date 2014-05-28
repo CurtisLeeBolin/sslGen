d64294647abb799487ef8caff9008d1d47fc6789
Curtis Lee Bolin <CurtisLeeBolin@gmail.com>
2014-05-28 16:19:11 -0500

    Create sslGen.sh

---
+++ sslGen.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# sslGen.sh
+#
+# Instructions:
+# ./sslGen.sh <domain>
+
+domain=$1
+
+openssl req -new -x509 -extensions v3_req -newkey rsa:8192 -nodes -days 3650 -subj "/O=$domain/CN=*.$domain/subjectAltName=DNS:*.$domain/" -out $domain.crt -keyout $domain.key
+chmod 600 $domain.key
