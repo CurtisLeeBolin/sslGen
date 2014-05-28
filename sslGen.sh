76bd78de98edac7f57462e8d0586dc6daf5ee376
Curtis Lee Bolin <CurtisLeeBolin@gmail.com>
2014-05-28 16:52:58 -0500

    Update sslGen.sh

--- sslGen.sh
+++ sslGen.sh
@@ -6,5 +6,7 @@
 
 domain=$1
 
-openssl req -new -x509 -extensions v3_req -newkey rsa:8192 -nodes -days 3650 -subj "/O=$domain/CN=*.$domain/subjectAltName=DNS:*.$domain/" -out $domain.crt -keyout $domain.key
+openssl req -new -x509 -extensions v3_req -newkey rsa:8192 -nodes -days 3650 \
+  -subj "/O=$domain/CN=*.$domain/subjectAltName=DNS:*.$domain/" \
+  -out $domain.crt -keyout $domain.key
 chmod 600 $domain.key
