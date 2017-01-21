303de6de11b653a08493d5a7083577fcc98d4cc3
Curtis Lee Bolin <CurtisLeeBolin@gmail.com>
2017-01-21 09:01:35 -0600

    Update sslGen.sh

--- sslGen.sh
+++ sslGen.sh
@@ -2,12 +2,20 @@
 # sslGen.sh
 #
 # Instructions:
-# ./sslGen.sh <domain>
+# ./sslGen.sh [domain]
 
-domain=$1
-
-openssl req -new -x509 -extensions v3_req -newkey rsa:8192 -nodes -days 3650 \
-  -subj "/O=$domain/CN=*.$domain/subjectAltName=DNS:*.$domain/" \
-  -out $domain.crt -keyout $domain.key
-openssl dhparam -out $domain.pem 2048
-chmod 600 $domain.key
+if [ -n "$1" ]; then
+  domain="$1"
+  mkdir $domain
+  openssl req -new -x509 -extensions v3_req -newkey rsa:8192 -nodes -days 3650 \
+    -subj "/O=$domain/CN=*.$domain/subjectAltName=DNS:*.$domain/" \
+    -out $domain/cert.pem -keyout $domain/cert.key
+  openssl dhparam -out $domain/dhparam 2048
+  chmod 600 $domain/cert.key
+else
+  openssl req -new -x509 -extensions v3_req -newkey rsa:8192 -nodes -days 3650 \
+    -subj "/O=/CN=/" \
+    -out cert.pem -keyout cert.key
+  openssl dhparam -out dhparam 2048
+  chmod 600 cert.key
+fi
